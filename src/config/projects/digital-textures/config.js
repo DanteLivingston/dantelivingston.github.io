a9a72c4fc7fa00f484b54247f2bfed4c0e561637
DanteLivingston <dantelivingston@gmail.com>
2020-10-30 14:02:59 -0700

    Update Digital Textures and Templates

--- src/config/projects/digital-textures/config.js
+++ src/config/projects/digital-textures/config.js
@@ -1,30 +1,96 @@
 import description from './description';
 
-import bigRedImage from './images/big-red-image.jpg';
-import smallOrangeImage from './images/small-orange-image.jpg';
-import smallRedImage from './images/small-red-image.png';
+import CorrugatedRoof_3D from './images/CorrugatedRoof_3D.png';
+import CorrugatedRoof from './images/CorrugatedRoof.png';
+import DamagedSideWalk_3D from './images/DamagedSideWalk_3D.png';
+import DamagedSideWalk from './images/DamagedSideWalk.jpeg';
+import JungleFloorTiles_3D from './images/JungleFloorTiles_3D.png';
+import JungleFloorTiles from './images/JungleFloorTiles.jpeg';
+import LavaFlow_3D from './images/LavaFlow_3D.png';
+import LavaFlow from './images/LavaFlow.jpeg';
+import MossyRocks_3D from './images/MossyRocks_3D.png';
+import MossyRocks from './images/MossyRocks.jpeg';
+import SandyBricks_3D from './images/SandyBricks_3D.png';
+import SandyBricks from './images/SandyBricks.png';
+import TunnelTiles_3D from './images/TunnelTiles_3D.png';
+import TunnelTiles from './images/TunnelTiles.png';
 
 const config = {
   slug: 'digital-textures',
-  title: 'Template Sample',
-  shortDescription: 'A sample with a template',
+  title: '3D Digital Textures',
+  shortDescription: 'test',
   description,
   type: 'digital',
   images: [
     {
-      url: bigRedImage,
-      title: 'magna vulputate',
+      url: CorrugatedRoof_3D,
+      title: 'Corrugated Roof Texture (3D)',
       description: 'lorem ipsum foobar magna vulputate',
     },
     {
-      url: smallOrangeImage,
+      url: CorrugatedRoof,
       title: 'ridiculus mus',
       description: 'lorem ipsum foobar ridiculus mus',
     },
     {
-      url: smallRedImage,
+      url: DamagedSideWalk_3D,
       title: 'quisque ut',
       description: 'lorem ipsum foobar quisque ut',
+    },
+    {
+      url: DamagedSideWalk,
+      title: 'magna vulputate',
+      description: 'lorem ipsum foobar magna vulputate',
+    },
+    {
+      url: JungleFloorTiles_3D,
+      title: 'magna vulputate',
+      description: 'lorem ipsum foobar magna vulputate',
+    },
+    {
+      url: JungleFloorTiles,
+      title: 'magna vulputate',
+      description: 'lorem ipsum foobar magna vulputate',
+    },
+    {
+      url: LavaFlow_3D,
+      title: 'magna vulputate',
+      description: 'lorem ipsum foobar magna vulputate',
+    },
+    {
+      url: LavaFlow,
+      title: 'magna vulputate',
+      description: 'lorem ipsum foobar magna vulputate',
+    },
+    {
+      url: MossyRocks_3D,
+      title: 'magna vulputate',
+      description: 'lorem ipsum foobar magna vulputate',
+    },
+    {
+      url: MossyRocks,
+      title: 'magna vulputate',
+      description: 'lorem ipsum foobar magna vulputate',
+    },
+    {
+      url: SandyBricks_3D,
+      title: 'magna vulputate',
+      description: 'lorem ipsum foobar magna vulputate',
+    },
+    {
+      url: SandyBricks,
+      title: 'magna vulputate',
+      description: 'lorem ipsum foobar magna vulputate',
+    },
+    {
+      url: TunnelTiles_3D,
+      title: 'magna vulputate',
+      description: 'lorem ipsum foobar magna vulputate',
+    },
+    {
+      url: TunnelTiles,
+      title: 'magna vulputate',
+      description: 'lorem ipsum foobar magna vulputate',
     },
   ],
 };
---
+++ src/config/projects/glitch-art/config.js
@@ -0,0 +1,32 @@
+import description from './description';
+
+import bigRedImage from './images/big-red-image.jpg';
+import smallOrangeImage from './images/small-orange-image.jpg';
+import smallRedImage from './images/small-red-image.png';
+
+const config = {
+  slug: 'glitch-art',
+  title: 'Glitch Art',
+  shortDescription: 'test',
+  description,
+  type: 'traditional',
+  images: [
+    {
+      url: bigRedImage,
+      title: 'magna vulputate',
+      description: 'lorem ipsum foobar magna vulputate',
+    },
+    {
+      url: smallOrangeImage,
+      title: 'ridiculus mus',
+      description: 'lorem ipsum foobar ridiculus mus',
+    },
+    {
+      url: smallRedImage,
+      title: 'quisque ut',
+      description: 'lorem ipsum foobar quisque ut',
+    },
+  ],
+};
+
+export default config;
