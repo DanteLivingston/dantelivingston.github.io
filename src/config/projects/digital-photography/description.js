304bd94a24359e67741eb4c6bdcafdf3dcb2cb58
DanteLivingston <dantelivingston@gmail.com>
2020-11-01 13:55:22 -0800

    Text edits

--- src/config/projects/digital-photography/description.js
+++ src/config/projects/digital-photography/description.js
@@ -1,4 +1,4 @@
-const description = `<p>This is a series of many works of digital photography I completed as a part of my Photo 40 class. It was my first exposure to digital photography outside of just using something as simple as my phone and was a great introduction to the medium. Through digital photography I found myself able to freely express my love for nature on a different level than ever before. Whether it is going on small nature hikes around my house, or taking the camera out with me whenever I go camping, I’ve found that there are always opportunities to take great photographs.</p>
+const description = `<p>This is a series of many works of digital photography I completed as a part of my Photo 40 class. It was my first exposure to digital photography outside of just using something as simple as my phone and was a great introduction to the medium. Through digital photography I found myself able to freely express my love for nature on a different level than ever before. Whether it is going on small hikes around my house, or taking the camera out with me whenever I go camping, I’ve found that there are always opportunities to take great photographs.</p>
 
 <p>I have also brought my digital photography into my digital illustration and 3D work. By utilizing my own photographs as reference, or background images for texture information I am more freely able to sidestep any potential copyright issues while still making use of the same techniques that many professional digital artists employ.</p>
 
@@ -6,7 +6,5 @@ const description = `<p>This is a series of many works of digital photography I 
 
 <hr/>
 
-<p>Link to <a rel='noopener noreferrer' target='_blank' href="https://www.usa.canon.com/internet/portal/us/home/support/details/cameras/eos-dslr-and-mirrorless-cameras/dslr/eos-rebel-t3/eos-rebel-t3">Canon Camera</a></p>
-
-<p>Link to <a rel='noopener noreferrer' target='_blank' href="https://www.adobe.com/products/photoshop-lightroom.html">Adobe Lightroom</a></p>`
+<ul><li><p>Link to <a rel='noopener noreferrer' target='_blank' href="https://www.usa.canon.com/internet/portal/us/home/support/details/cameras/eos-dslr-and-mirrorless-cameras/dslr/eos-rebel-t3/eos-rebel-t3">Canon Camera</a></p></li><li><p>Link to <a rel='noopener noreferrer' target='_blank' href="https://www.adobe.com/products/photoshop-lightroom.html">Adobe Lightroom</a></p></li></ul>`
 export default description
