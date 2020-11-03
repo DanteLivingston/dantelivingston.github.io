1384ee12e90d285c70a2074096f5b08e39214bb9
DanteLivingston <dantelivingston@gmail.com>
2020-11-03 09:39:44 -0800

    add digitial illustrations

--- src/config/projects/digital-illustrations/description.js
+++ src/config/projects/digital-illustrations/description.js
@@ -1,22 +1,10 @@
-const description = `<h1>H1 Header</h1>
+const description = `<p>This is a compilation of the first digital illustration work I have ever attempted. I have always wanted to push towards digital illustration and to improve my skills. However after plenty of attempts with little success translating what I knew from traditional drawing into digital results I would quickly give up. Eventually, my parents ended up getting an Ipad with an <strong><a rel='noopener noreferrer' target='_blank' href="https://www.apple.com/apple-pencil/">Apple Pencil</a></strong> for themselves, only for me to find it and start drawing. While before my digital art never looked as good as what I could accomplish in traditional, I was able to fully realize myself digitally within these pieces. They were drawn with an Apple pencil through the program <strong><a rel='noopener noreferrer' target='_blank' href="https://procreate.art/">Procreate</a></strong>, as Photoshop on the Ipad is nowhere near as good as the desktop version from what I’ve seen. The characters themselves are all from the Japanese Anime “Kono Subarashii Sekai ni Shukufuku wo!” which translated to “God’s Blessing on this Wonderful world”, I do not claim for any of their designs to be my own or original. I have always loved the style of animation in it’s colorful nature and fluidity, as well as it’s simplicity.</p>
 
-<h2>H2</h2>
+<p>I first drew Aqua, the goddess with blue hair using the show’s promotional art as reference and made an effort to make a study of it by mimicking things like cell shading, poses, etc. She was the first piece of digital illustration art I have ever made and ultimately, I think she turned out well, but there were definitely lessons taught to me through the process of drawing. I would later iterate and experiment with the next two pieces, the red robed girl, Megumin and yellow armored crusader Darkness. For both of them I attempted to use a more realistic shading method to make them much less stylized. I also created unique poses for them to give them more motion and dynamism as well as to add my own flair. For Kazuma, the green rogue is at the moment my most recent piece, I pushed what I had learned previously even further. I wanted to experiment with more lighting options including using layers to my advantage as well as to really refine my core shading to create a more realistic look. I thought he was fairly successful in the end and shows off some of the growth I have been able to achieve with the limited time I have been working on digital illustration as a whole.</p>
 
-<h3>H3</h3>
-
-<p>I am <strong>bold</strong> text</p>
-
-<p>I am <em>italicized</em> text</p>
-
-<blockquote><p>I am block quote</p></blockquote>
-
-<ol><li>first</li><li>second</li><li><p>third</p></li><li><p>first</p></li><li>second</li><li>third</li></ol>
-
-<p><code>i am code</code></p>
-
-<p>Horizontal rule below</p>
+<p>Kazuma as well as Darkness and the group image are all works in progress at the moment that I would like to iterate on further. However I wanted to showcase them all together for composition purposes as well as to show my growth.</p>
 
 <hr/>
 
-<p>Link to <a rel='noopener noreferrer' target='_blank' href="http://www.google.com">Google</a></p>`
+<ul><li><p>Link to the <a rel='noopener noreferrer' target='_blank' href="https://konosuba.fandom.com/wiki/Kono_Subarashii_Sekai_ni_Shukufuku_wo!_Wiki">KonoSubarashii Wiki</a></p></li><li><p>Link to <a rel='noopener noreferrer' target='_blank' href="https://www.apple.com/apple-pencil/">Apple Pencil</a></p></li><li><p>Link to <a rel='noopener noreferrer' target='_blank' href="https://procreate.art/">Procreate</a></p></li></ul>`
 export default description
