07ff827cd75eda6f3a448893fc6f965f86b1a9ec
DanteLivingston <dantelivingston@gmail.com>
2021-10-12 09:58:08 -0700

    Test

--- src/config/projects/photoshop-projects/config.js
+++ src/config/projects/photoshop-projects/config.js
@@ -3,7 +3,7 @@ import MiniatureEffect from './images/miniature.png';
 import SnowyMountain from './images/snowmountain.jpeg';
 
 const config = {
-  enabled: false,
+  enabled: true,
   slug: 'photoshop-projects',
   title: 'Photoshop Skill Projects',
   shortDescription: 'Individual Photoshop Projects I have completed as a part of my Art 74 Class.',
