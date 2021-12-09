4c31b6b8bec942a0bfbe209085f265ae6e75e1ac
DanteLivingston <dantelivingston@gmail.com>
2021-12-08 23:03:01 -0800

    Website Updates

--- src/config/projects/index.js
+++ src/config/projects/index.js
@@ -1,32 +1,29 @@
 //digital
-import digitalTextures from './digital-textures/config';
-import videoEditingProject from './video-editing-project/config';
-import virtualGallery from './virtual-gallery/config';
-import interactiveAudioProject from './interactive-audio-project/config';
-import glitchArt from './glitch-art/config';
-import photoShopProjects from './photoshop-projects/config';
-import fakeProduct from './fake-product/config';
-import digitalIllustration from './digital-illustrations/config';
-// import videoGallery from './video-gallery/config';
-
-//inspiration
-import hayaoMiyazaki from './hayao-miyazaki/config';
 import aaronBlaise from './aaron-blaise/config';
 import corridorDigital from './corridor-digital/config';
-import fengZhu from './feng-zhu/config';
-import jeanMoebius from './jean-moebius/config';
-import kimJungGi from './kim-jung-gi/config';
-
+import digitalIllustration from './digital-illustrations/config';
 //traditional
 import digitalPhotography from './digital-photography/config';
+import digitalTextures from './digital-textures/config';
+import fakeProduct from './fake-product/config';
+import fengZhu from './feng-zhu/config';
+import glitchArt from './glitch-art/config';
+// import videoGallery from './video-gallery/config';
+//inspiration
+import hayaoMiyazaki from './hayao-miyazaki/config';
+import interactiveAudioProject from './interactive-audio-project/config';
+import jeanMoebius from './jean-moebius/config';
+import kimJungGi from './kim-jung-gi/config';
 import minimalist from './minimalist-art/config';
+import photoShopProjects from './photoshop-projects/config';
 import plasticSculpture from './plastic-and-metal-sculpture/config';
-import traditionalInk from './traditional-ink-drawing/config';
-import wireSculpture from './WireSculpture/config';
-
 //other
 //import template from './template/config';
 import tools from './tools/config';
+import traditionalInk from './traditional-ink-drawing/config';
+import videoEditingProject from './video-editing-project/config';
+import virtualGallery from './virtual-gallery/config';
+import wireSculpture from './WireSculpture/config';
 
 const projects = [
   // Digital Art
@@ -34,7 +31,6 @@ const projects = [
   glitchArt,
   digitalIllustration,
   photoShopProjects,
-  digitalPhotography,
   fakeProduct,
   // videoGallery,
   videoEditingProject,
@@ -50,6 +46,7 @@ const projects = [
   kimJungGi,
   // Traditional Art
   minimalist,
+  digitalPhotography,
   plasticSculpture,
   traditionalInk,
   wireSculpture,
